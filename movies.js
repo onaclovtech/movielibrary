4f2f65033677d0ee18710245d4a2fdcf30fe45f1
Tyson Bailey <onaclov2000+github@gmail.com>
2014-08-07 21:19:11 -0600

    Update movies.js

--- movies.js
+++ movies.js
@@ -1,12 +1,18 @@
+/*
+* Movie Library v0.0.1
+*/
    var app = angular.module("myapp", ["firebase", "video"])
        .controller('MyController', ['$scope', '$video', '$firebase',
            function($scope, $video, $firebase) {
                var FB = "";
-               var api_key = "";
                var ref = new Firebase(FB);
                $scope.movies = $firebase(ref);
+               var FB2 = "";
+               var ref2 = new Firebase(FB2);
+               $scope.devices = $firebase(ref2);
                $scope.edit = false;
                $scope.details = [false, false, false, false, false];
+               $scope.play = true;
                var resetDetails = function() {
                    $scope.details[0] = false;
                    $scope.details[1] = false;
@@ -14,14 +20,35 @@
                    $scope.details[3] = false;
                    $scope.details[4] = false;
                }
+               $scope.remove = function(id) {
+                   console.log(id.$ref());
+               }
+
+               $scope.rokuRemote = function(operation) {
+                   //somehow figure out what fb url is the device, that's how we control it.
+                   var FB3 = "";
+                   var ref3 = new Firebase(FB3);
+                   $scope.temp = $firebase(ref3);
+                   console.log($scope.temp);
+                   $scope.temp.$update({
+                       "state": operation
+                   });
+                   if (operation == "play") {
+                       $scope.play = !$scope.play;
+                   }
+                   if (operation == "pause") {
+                       $scope.play = !$scope.play;
+                   }
+               }
                $scope.selectDVD = function(index) {
+
                    $scope.title = $scope.data["movies"][index]["title"];
                    $scope.thumbnail = $scope.data["movies"][index]["posters"]["thumbnail"].replace("_tmb", "_det");
                    $scope.rating = $scope.data["movies"][index]["ratings"]["critics_rating"];
                    $scope.filename = $scope.data["movies"][index]["title"].replace(/ /g, ".") + '.m4v';
                };
                $scope.findDvd = function(e) {
-                   var response = $video.search(api_key, $scope.dvd, '5');
+                   var response = $video.search('yaf6teansrndryqqa9tvdnv4', $scope.dvd, '5');
                    response
                        .success(function(data, status) {
                            console.log('SUCCESS' + data);
